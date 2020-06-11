6670e2bdc19d0bc26f9830ba375a26c7322dafce
Mike Williams <mumakin@gmail.com>
2020-06-10 22:31:03 -0600

    object types

--- scratch/types/app.ts
+++ scratch/types/app.ts
@@ -1,8 +1,15 @@
 function add(n1: number, n2: number) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 3.14159;
 const result = add(number1, number2);
 console.log(result);
+
+const person = {
+  name: "Mike",
+  age: 37,
+};
+
+console.log(person);
