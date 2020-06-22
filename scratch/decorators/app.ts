457febb84e340fab5497f98709cc979eb5841f38
Mike Williams <mumakin@gmail.com>
2020-06-21 21:45:38 -0600

    using decorators

--- scratch/decorators/app.ts
+++ scratch/decorators/app.ts
@@ -1,1 +1,43 @@
-console.log('Your code goes here!');
+function Logger(logMessage: string) {
+  return function Logger(constructor: Function) {
+    console.log(logMessage);
+    console.log(constructor);
+  };
+}
+
+@Logger('Parsing the person')
+class Person {
+ name = 'Max'   ;
+
+ constructor() {
+     console.log("Creating person object");
+ }
+}
+
+const person = new Person();
+
+console.log(person);
+
+class Course {
+    title: string;
+    price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event =>{
+    event.preventDefault();
+
+    const titleElem = document.getElementById('title') as HTMLInputElement;
+    const priceElem = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleElem.value;
+    const price = +priceElem.value;
+
+    const createdCourse = new Course(title, price);
+    console.log(createdCourse);
+})
