8804f2510b6ef0eba8f07a3476ee671ae242a10d
Mike Williams <mumakin@gmail.com>
2020-06-21 21:57:26 -0600

    decorators as validators

--- scratch/decorators/app.ts
+++ scratch/decorators/app.ts
@@ -18,8 +18,49 @@ const person = new Person();
 
 console.log(person);
 
+interface Validateconfig {
+    [property: string]:{
+        [validatableProp: string]: string[];
+    }
+}
+
+const registeredValidators: Validateconfig = {};
+
+function Required(target: any, propertyName: string) {
+    registeredValidators[target.constructor.name] = {
+        [propertyName]: ['required']
+    }
+}
+
+function PostitiveNumber(target: any, propertyName: string) {
+    registeredValidators[target.constructor.name] = {
+        [propertyName]: ['positive']
+    }
+}
+
+function validate(obj: any) {
+    const objectValidatorConfig = registeredValidators[obj.constructor.name];
+    if(!objectValidatorConfig) {
+        return true;
+    }
+    for(const prop in objectValidatorConfig) {
+        for(const validator of objectValidatorConfig[prop]){
+            switch(validator){
+                case 'required':
+                    return !!obj[prop];
+                case 'positive':
+                    return obj[prop] > 0;
+            }
+        }
+        return true;
+    }
+
+}
+
 class Course {
+    @Required
     title: string;
+    @PostitiveNumber
     price: number;
 
     constructor(t: string, p: number) {
@@ -39,5 +80,9 @@ courseForm.addEventListener('submit', event =>{
     const price = +priceElem.value;
 
     const createdCourse = new Course(title, price);
+    if(!validate(createdCourse)) {
+        alert('INVALID INPUT PLEASE TRY AGAIN!');
+        return;
+    }
     console.log(createdCourse);
 })
