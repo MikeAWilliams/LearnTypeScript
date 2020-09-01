c6947bca6697f86d6450484f7fef3891ffbe287d
Michael Williams <mwilliams@alteryx.com>
2020-09-01 10:08:02 -0600

    substitute const for var

--- kata/fizzbuzz/src/fizzbuzz.ts
+++ kata/fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export function fizzbuzz(num: number) {
-   var divisbileBy3 = (num % 3 == 0);
-   var divisibleBy5 = (num % 5 == 0);
+   const divisbileBy3 = (num % 3 == 0);
+   const divisibleBy5 = (num % 5 == 0);
    if(divisibleBy5 && divisbileBy3) {
       return "FizzBuzz";
    }
