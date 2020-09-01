e725822ce5b46c2fc46b7c4a4f2170e99db90e7a
Michael Williams <mwilliams@alteryx.com>
2020-09-01 08:11:04 -0600

    1 and multiples of 3

--- kata/fizzbuzz/__tests__/fizzbuzz.test.ts
+++ kata/fizzbuzz/__tests__/fizzbuzz.test.ts
@@ -2,4 +2,11 @@ import {fizzbuzz} from "../src";
 
 test('divisible by 3 returns Fizz', () => {
   expect(fizzbuzz(3)).toBe("Fizz");
+  expect(fizzbuzz(6)).toBe("Fizz");
+  expect(fizzbuzz(9)).toBe("Fizz");
+  expect(fizzbuzz(12)).toBe("Fizz");
+});
+
+test('not divisible by 3, 5, or 15 returns the number', () => {
+  expect(fizzbuzz(1)).toBe("1");
 });
