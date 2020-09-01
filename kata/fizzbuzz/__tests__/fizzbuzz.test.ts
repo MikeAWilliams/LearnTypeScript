115837917bc5bb9401ddd8c5628f8bbc98bc4d08
Michael Williams <mwilliams@alteryx.com>
2020-09-01 08:14:09 -0600

    divisible by 5

--- kata/fizzbuzz/__tests__/fizzbuzz.test.ts
+++ kata/fizzbuzz/__tests__/fizzbuzz.test.ts
@@ -7,6 +7,18 @@ test('divisible by 3 returns Fizz', () => {
   expect(fizzbuzz(12)).toBe("Fizz");
 });
 
+test('divisible by 5 returns Buzz', () => {
+  expect(fizzbuzz(5)).toBe("Buzz");
+  expect(fizzbuzz(10)).toBe("Buzz");
+  expect(fizzbuzz(20)).toBe("Buzz");
+  expect(fizzbuzz(25)).toBe("Buzz");
+});
+
 test('not divisible by 3, 5, or 15 returns the number', () => {
   expect(fizzbuzz(1)).toBe("1");
+  expect(fizzbuzz(2)).toBe("2");
+  expect(fizzbuzz(4)).toBe("4");
+  expect(fizzbuzz(7)).toBe("7");
+  expect(fizzbuzz(16)).toBe("16");
+  expect(fizzbuzz(122)).toBe("122");
 });
