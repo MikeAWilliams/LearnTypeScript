191e96cc9097ea5cf245af658a492b8f93682a0d
Michael Williams <mwilliams@alteryx.com>
2020-09-02 09:27:00 -0600

    add watch command and also make primefactors(2) pass

--- kata/primefactors/__tests__/primefactors.test.ts
+++ kata/primefactors/__tests__/primefactors.test.ts
@@ -3,3 +3,7 @@ import {primefactors} from "../src";
 test('primefactors of 1 is empty', () => {
    expect(primefactors(1)).toEqual([]);
 });
+
+test('primefactors of 2 is 2', () => {
+   expect(primefactors(2)).toEqual([2]);
+});
--- kata/primefactors/src/primefactors.ts
+++ kata/primefactors/src/primefactors.ts
@@ -1,3 +1,6 @@
 export function primefactors(num: number) {
+   if(2 == num) {
+      return [2];
+   }
    return [] as number[];
 }
