d6b12b1865193008870c34624acca65a5a70741b
Michael Williams <mwilliams@alteryx.com>
2020-09-02 09:41:25 -0600

    8 passes

--- kata/primefactors/__tests__/primefactors.test.ts
+++ kata/primefactors/__tests__/primefactors.test.ts
@@ -15,3 +15,11 @@ test('primefactors of 3 is 3', () => {
 test('primefactors of 4 is 2,2', () => {
    expect(primefactors(4)).toEqual([2,2]);
 });
+
+test('primefactors of 6 is 2,3', () => {
+   expect(primefactors(6)).toEqual([2,3]);
+});
+
+test('primefactors of 8 is 2,2,2', () => {
+   expect(primefactors(8)).toEqual([2,2,2]);
+});
--- kata/primefactors/src/primefactors.ts
+++ kata/primefactors/src/primefactors.ts
@@ -1,6 +1,6 @@
 export function primefactors(num: number) {
    let result: number[] = [];
-   if(num % 2 == 0){
+   while(num % 2 == 0){
       result.push(2);
       num /= 2;
    }
