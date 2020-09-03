2fdf9fc850a8dde90d4e741a6fd20d3357e39dda
Michael Williams <mwilliams@alteryx.com>
2020-09-03 14:53:27 -0600

    first virtical test

--- kata/tictactoe/__tests__/tictactoe.test.ts
+++ kata/tictactoe/__tests__/tictactoe.test.ts
@@ -16,4 +16,52 @@ it('after one move the game is not over and the next player is O', () => {
    expect(game.isOver()).toBeFalsy();
    expect(game.getPlayerTakingThisTurn()).toBe("O");
 });
+
+it('x wins top', () => {
+   const xRow = 0;
+   const oRow = 1;
+   game.takeTurn(0,xRow);
+   game.takeTurn(1,oRow);
+   game.takeTurn(1,xRow);
+   game.takeTurn(0,oRow);
+   game.takeTurn(2,xRow);
+   expect(game.isOver()).toBeTruthy();
+   expect(game.getWinner()).toBe("X");
+});
+
+it('x wins middle row', () => {
+   const xRow = 1;
+   const oRow = 0;
+   game.takeTurn(0,xRow);
+   game.takeTurn(1,oRow);
+   game.takeTurn(1,xRow);
+   game.takeTurn(0,oRow);
+   game.takeTurn(2,xRow);
+   expect(game.isOver()).toBeTruthy();
+   expect(game.getWinner()).toBe("X");
+});
+
+it('x wins bottom row', () => {
+   const xRow = 2;
+   const oRow = 0;
+   game.takeTurn(0,xRow);
+   game.takeTurn(1,oRow);
+   game.takeTurn(1,xRow);
+   game.takeTurn(0,oRow);
+   game.takeTurn(2,xRow);
+   expect(game.isOver()).toBeTruthy();
+   expect(game.getWinner()).toBe("X");
+});
+
+it('x wins first column', () => {
+   const xCol = 0;
+   const oCol = 1;
+   game.takeTurn(xCol, 0);
+   game.takeTurn(oCol, 0);
+   game.takeTurn(xCol, 1);
+   game.takeTurn(oCol, 1);
+   game.takeTurn(xCol, 2);
+   expect(game.isOver()).toBeTruthy();
+   expect(game.getWinner()).toBe("X");
+});
 });
--- kata/tictactoe/src/tictactoe.ts
+++ kata/tictactoe/src/tictactoe.ts
@@ -3,18 +3,20 @@ export default interface IGame {
   getWinner(): string;
   getPlayerTakingThisTurn(): string;
   takeTurn(x:number, y:number):void;
-  getBoardState(): Array<Array<string>>;
+  getBoardState(): string; 
 }
 
 export class Game implements IGame {
    private board: Array<Array<string>> = [[" ", " ", " "],[" ", " ", " "], [" ", " ", " "] ];
    private isXturn: boolean = true;
+   private winner: string = null;
+
   isOver(): boolean {
-      return false;
+      return null != this.winner;
   }
 
   getWinner(): string{
-     throw new Error('Not Implmemented');
+     return this.winner;
   }
 
   getPlayerTakingThisTurn(): string {
@@ -25,12 +27,54 @@ export class Game implements IGame {
   }
 
   takeTurn(x:number, y:number):void {
-     this.board[y][x] = this.getPlayerTakingThisTurn();
+     const player = this.getPlayerTakingThisTurn();
+     this.board[y][x] = player;
      this.isXturn = !this.isXturn;
+     this.updateWinner(player);
   }
 
-  getBoardState(): Array<Array<string>> {
-     return Array<Array<string>>();
+  getBoardState(): string {
+     let result = "";
+     for(const row of this.board){
+        for(const position of row) {
+           result += position;
+        }
+        result += "\n";
+     }
+     return result;
   }
 
+  private updateWinner(player: string): void {
+      if(this.playerWon(player)){
+         this.winner = player;
+      }
+  }
+
+  private playerWon(player:string): boolean {
+      return this.playerWonHorizontal(player) || this.playerWonVertical(player);
+  }
+
+  private playerWonVertical(player: string): boolean {
+     return this.playerWonInCol(player, 0) || this.playerWonInCol(player, 1) || this.playerWonInCol(player, 2);
+  }
+
+  private playerWonHorizontal(player:string): boolean {
+     return this.playerWonInRow(player, 0) || this.playerWonInRow(player, 1) || this.playerWonInRow(player, 2);
+  }
+
+  private playerWonInCol(player:string, colIndex:number):boolean {
+     return this.playerHoldsPosition(player, 0, colIndex) && 
+      this.playerHoldsPosition(player, 1, colIndex) && 
+      this.playerHoldsPosition(player, 2, colIndex);
+  }
+
+  private playerWonInRow(player:string, rowIndex:number):boolean {
+     return this.playerHoldsPosition(player, rowIndex, 0) && 
+      this.playerHoldsPosition(player, rowIndex, 1) && 
+      this.playerHoldsPosition(player, rowIndex, 2);
+  }
+
+  private playerHoldsPosition(player:string, rowIndex:number, columnIndex:number):boolean {
+     return this.board[rowIndex][columnIndex] == player;
+  }
 }
