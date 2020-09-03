c0d1e0dec07b9cd0153db007d79af58a1fab8810
Michael Williams <mwilliams@alteryx.com>
2020-09-03 09:22:56 -0600

    hello my friend

--- kata/greeting/__tests__/greeting.test.ts
+++ kata/greeting/__tests__/greeting.test.ts
@@ -4,3 +4,8 @@ test('hello name', () => {
    expect(greet("Bob")).toEqual("Hello, Bob.");
    expect(greet("Susan")).toEqual("Hello, Susan.");
 });
+
+test('hello my friend', () => {
+   expect(greet("")).toEqual("Hello, my friend.");
+   expect(greet(null)).toEqual("Hello, my friend.");
+});
--- kata/greeting/src/greeting.ts
+++ kata/greeting/src/greeting.ts
@@ -1,3 +1,6 @@
 export function greet(name: string) {
+   if(!name){
+      return "Hello, my friend.";
+   }
    return "Hello, " + name + ".";
 }
